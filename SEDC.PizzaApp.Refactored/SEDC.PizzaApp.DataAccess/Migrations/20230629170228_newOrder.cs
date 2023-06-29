6ed1b769a517180b17a467e2907a002d067526e6
Stefan Trajanoski <Stefan.Trajanoski@seavus.com>
2023-06-29 20:10:31 +0200

    Inital commit for Pizza app project

---
+++ SEDC.PizzaApp.Refactored/SEDC.PizzaApp.DataAccess/Migrations/20230629170228_newOrder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SEDC.PizzaApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class newOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PizzaOrder",
+                columns: new[] { "Id", "NumberOfPizzas", "OrderId", "PizzaId", "PizzaSize", "Price" },
+                values: new object[] { 4, 2, 2, 3, 2, 400.0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PizzaOrder",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
---
+++ SEDC.PizzaApp.Refactored/SEDC.PizzaApp.DataAccess/Migrations/20230629170759_usersRepositorynewFInalVersion.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SEDC.PizzaApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class usersRepositorynewFInalVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsersId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "UsersTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersTable", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UsersId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "UsersId",
+                value: null);
+
+            migrationBuilder.InsertData(
+                table: "UsersTable",
+                columns: new[] { "Id", "Address", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "Address1", "Afroddasdasita", "Bedsadsadasle" },
+                    { 2, "Adddasasddsaress2", "Stdasasefan", "Tradasdasjanoski" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UsersId",
+                table: "Orders",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_UsersTable_UsersId",
+                table: "Orders",
+                column: "UsersId",
+                principalTable: "UsersTable",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_UsersTable_UsersId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "UsersTable");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UsersId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UsersId",
+                table: "Orders");
+        }
+    }
+}
